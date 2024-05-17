b2cd3a392f3c7caba4eab22f1d47c93253682963
AGCode23 <165239509+AGCode23@users.noreply.github.com>
2024-05-17 20:37:13 +0800

    refactor fizzbuzz.js

--- JavaScript/FizzBuzz.js
+++ JavaScript/FizzBuzz.js
@@ -4,17 +4,14 @@ function fizzBuzz() {
   if (count % 3 === 0) {
     if (count % 3 === 0 && count % 5 === 0) {
       output.push("FizzBuzz");
-      count++;
     } else {
       output.push("Fizz");
-      count++;
     }
   } else if (count % 5 === 0) {
     output.push("Buzz");
-    count++;
   } else {
     output.push(count);
-    count++;
   }
+  count++;
   console.log(output);
 }
